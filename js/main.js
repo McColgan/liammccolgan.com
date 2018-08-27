bc37ae4c0a4b9cdfc5bef10598f596a0042b12e6
Liam <l.mccolgan.connect@gmail.com>
2018-08-27 20:40:33 +0100

    added header,  html file structures

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    // Nav-Icon animation
+    $('#nav_icon').click(function(){
+        $('.nav_container').toggleClass('nav_container--open', 300);
+        $(this).toggleClass('open');
+    });
+
+    // Hiding header on scroll
+    let prevScrollPos = window.pageYOffset;
+    // Checking to see if screen width matches media query for desktop header
+    let screenWidth = window.matchMedia( "(min-width: 800px)" );
+    
+    if (screenWidth.matches){
+         window.onscroll = function(){
+            let currentScrollPos = window.pageYOffset;
+            if (prevScrollPos > currentScrollPos){
+                document.getElementById("header_container").style.top = "0";
+            } else{
+                document.getElementById("header_container").style.top = "-130px";
+            }
+        
+            prevScrollPos = currentScrollPos;
+            }
+        }
+});
